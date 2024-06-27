17ed1ce7f44f439acdf85b0eb8acc6742215d81c
addiradical <ajanzen-winter2@rrc.ca>
2024-06-27 12:30:51 -0500

    added implemented controller action

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,7 +1,13 @@
 class PokemonController < ApplicationController
+  require 'httparty'
+
   def index
+    response = HTTParty.get('https://pokeapi.co/api/v2/pokemon?limit=10')
+    @pokemons = response.parsed_response['results']
   end
 
   def show
+    response = HTTParty.get("https://pokeapi.co/api/v2/pokemon/#{params[:id]}")
+    @pokemon = response.parsed_response
   end
 end
